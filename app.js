e292273619f3fa63b6e9d8ae96a47b6c92b66a04
Joao Gomes <joaopfg.2002@gmail.com>
2020-05-25 20:40:00 +0100

    b

--- app.js
+++ app.js
@@ -4,40 +4,30 @@ mongoose.set('useUnifiedTopology', true);
 mongoose.connect('mongodb://localhost:27017/fruitsDB', { useNewUrlParser: true })
 
 const fruitSchema = new mongoose.Schema({
-      name: String,
-      rating: Number,
+      name: {
+        type: String,
+        required: [true, "Por favor insira nome"],
+      },
+      rating: {
+        type: Number,
+        min: 1,
+        max: 10
+      },
       review: String
 });
 
 const Fruit = mongoose.model('Fruit', fruitSchema);
 
 const fruit = new Fruit({
-  name: "Apple",
-  rating: 7,
-  review: "Good"
-  
-}
-);
-
-const kiwi = new Fruit({
-  name: "Kiwi",
-  rating: 9,
-  review: "Nice"
-});
-
-const banana = new Fruit({
-  name: "Banana",
-  rating: 10,
-  review: "Monkey Likes"
-});
-
-const orange = new Fruit({
-  name: "Orange",
+  name: "Pessego",
   rating: 6,
-  review: "Not so good"
+  review: "Never prove it"
+  
 });
 
-      const PersonSchema = new mongoose.Schema({
+// fruit.save();
+
+ const PersonSchema = new mongoose.Schema({
         name: String,
         age: Number,
         
@@ -50,19 +40,7 @@ const orange = new Fruit({
         
       }
       );
-      // person.save();
 
-// Fruit.insertMany([banana, orange] , function (err) {
-
-//   if(err){
-//     console.log(err);
-    
-//   }else{
-//     console.log("Success");
-    
-//   }
-  
-// });
 Fruit.find(function (err, fruits) {
     if(err){
       console.log(err);
